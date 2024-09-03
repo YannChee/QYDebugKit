cde127e0841ff1bd4058116aa3bb512e6ef09769
XingYing <yannchee@163.com>
2024-09-03 11:21:51 +0800

    Realize the automatic display and hiding of the navigation bar

--- QYDebugKit/Classes/QYNavigationController.m
+++ QYDebugKit/Classes/QYNavigationController.m
@@ -7,18 +7,45 @@
 
 #import "QYNavigationController.h"
 
-@interface QYNavigationController ()
-
-@end
-
 @implementation QYNavigationController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    // 检查当前的视图控制器是否是根视图控制器
+    if (self.topViewController == self.viewControllers.firstObject) {
+        // 如果是根视图控制器，隐藏导航栏
+        [self setNavigationBarHidden:YES animated:NO];
+    } else {
+        // 否则，显示导航栏
+        [self setNavigationBarHidden:NO animated:NO];
+    }
 }
 
-//- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
-//    return DoraemonManager.shareInstance.supportedInterfaceOrientations;
-//}
+- (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
+    [super pushViewController:viewController animated:animated];
+    
+    // 在推送后进行更新
+    [self updateNavigationBarVisibility];
+}
+
+- (UIViewController *)popViewControllerAnimated:(BOOL)animated {
+    UIViewController *poppedViewController = [super popViewControllerAnimated:animated];
+    
+    // 在弹出后进行更新
+    [self updateNavigationBarVisibility];
+    
+    return poppedViewController;
+}
+
+- (void)updateNavigationBarVisibility {
+    if (self.topViewController == self.viewControllers.firstObject) {
+        // 如果当前视图控制器是根视图控制器，隐藏导航栏
+        [self setNavigationBarHidden:YES animated:NO];
+    } else {
+        // 否则，显示导航栏
+        [self setNavigationBarHidden:NO animated:NO];
+    }
+}
+
 @end
